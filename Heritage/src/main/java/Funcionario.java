cb6deda8d84681b060ae63096267be7f6b31c9f8
ThePokerJoker <svalcacio@gmail.com>
2019-09-12 10:38:08 -0300

    Upload do Java (Tentativa quase Falha)

---
+++ Heritage/src/main/java/Funcionario.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Funcionario {
+    public String Nome;
+    public int Idade;
+    public String Sexo;
+    public String Formação;
+    public float Salário;
+    
+}
+
+public class Professor extends Funcionario{
+
+    public String Matéria;
+    
+        
+}
+
+public static void main{
+    System.out.print("Tentamos.");
+
+}
